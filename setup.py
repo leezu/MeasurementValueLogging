6ea6306dff61d3b0895484c9a82adbff24f4427a
leezu <leonard@lausen.nl>
2013-03-05 20:55:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,15 @@ setup(name="MeasurementValueLogging",
 	url="https://github.com/leezu/measurement-value-logging",
 	packages = ['MeasurementValueLogging', 'MeasurementValueLogging.devices'],
 	requires = ["pyserial", "PyQt"],
+	classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Environment :: Win32 (MS Windows)',
+          'Environment :: X11 Applications :: Qt',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python'
 	)
