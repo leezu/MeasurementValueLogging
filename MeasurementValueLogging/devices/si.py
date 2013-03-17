eb45a0be7941384c1021fc6fa86c0742b200000a
leezu <leonard@lausen.nl>
2013-03-17 21:28:44 +0100

    Bugfix in devices.si.splitNumber(x) when x == 0

--- MeasurementValueLogging/devices/si.py
+++ MeasurementValueLogging/devices/si.py
@@ -27,8 +27,10 @@ def splitNumber(x):
     (2.5, 7)
 
     """
-
-    return round(x * pow(10, -1 * math.floor(math.log(x,10))), 15), math.floor(math.log(x, 10))
+    try:
+        return round(x * pow(10, -1 * math.floor(math.log(x,10))), 15), math.floor(math.log(x, 10))
+    except ValueError: # If x == 0
+        return 0.0, 0.0
 
 def getNumberPrefix(x):
     """Return a tuple containing x splitted to a number and SI-Prefix.
