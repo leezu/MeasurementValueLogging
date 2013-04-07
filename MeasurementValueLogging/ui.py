fd6dce80464746c8c71def7348e2f0426f9bd570
leezu <leonard@lausen.nl>
2013-04-07 11:52:06 +0200

    Fix some typos

--- MeasurementValueLogging/ui.py
+++ MeasurementValueLogging/ui.py
@@ -320,19 +320,19 @@ class DeviceSettingsDialog(QtGui.QDialog):
         self.is1.setValue(self.settings.value("calibration/" +
             self.slotComboBox.currentText() + "is1", 0).toInt()[0])
         self.should1.setValue(self.settings.value("calibration/" +
-            self.slotComboBox.currentText() + "should1", 1).toInt()[0])
+            self.slotComboBox.currentText() + "should1", 0).toInt()[0])
         self.is2.setValue(self.settings.value("calibration/" +
             self.slotComboBox.currentText() + "is2", 1).toInt()[0])
         self.should2.setValue(self.settings.value("calibration/" +
             self.slotComboBox.currentText() + "should2", 1).toInt()[0])
         self.is1Prefix.setCurrentIndex(self.settings.value("calibration/" +
-            self.slotComboBox.currentText() + "is1PrefixIndex", "").toInt()[0])
+            self.slotComboBox.currentText() + "is1PrefixIndex", 3).toInt()[0])
         self.should1Prefix.setCurrentIndex(self.settings.value("calibration/" +
-            self.slotComboBox.currentText() + "should1PrefixIndex", "").toInt()[0])
+            self.slotComboBox.currentText() + "should1PrefixIndex", 3).toInt()[0])
         self.is2Prefix.setCurrentIndex(self.settings.value("calibration/" +
-            self.slotComboBox.currentText() + "is2PrefixIndex", "").toInt()[0])
+            self.slotComboBox.currentText() + "is2PrefixIndex", 3).toInt()[0])
         self.should2Prefix.setCurrentIndex(self.settings.value("calibration/" +
-            self.slotComboBox.currentText() + "should2PrefixIndex", "").toInt()[0])
+            self.slotComboBox.currentText() + "should2PrefixIndex", 3).toInt()[0])
 
         self.slope.setValue(self.settings.value("calibration/" +
             self.slotComboBox.currentText() + "slope", 1).toInt()[0])
