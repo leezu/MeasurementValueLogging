a44cb45e63baf9ebf7b64ee662e93f3a94f42234
leezu <leonard@lausen.nl>
2013-05-13 20:09:04 +0200

    Version 0.4.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Please see http://leezu.github.com/MeasurementValueLogging/ for documentation an
 
 
 setup(name="MeasurementValueLogging",
-	version="0.4.0",
+	version="0.4.1",
 	description="Display measurement values from various serial devices.",
 	long_description=desc,
 	author="Leonard Lausen",
